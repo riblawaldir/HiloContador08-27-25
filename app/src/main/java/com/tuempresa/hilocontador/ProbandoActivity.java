51b17a13e7eb9e689463e60b91d55ce855e7c174
riblawaldir <trancosowaldir@gmail.com>
2025-08-27 19:41:18 -0400

    Refactor: Implement toggling traffic light
    This commit introduces functionality to toggle a traffic light image
    between red and black on button click.
    
    Key changes:
    - Added `foco_rojo.xml` drawable for the red light.
    - Modified `ProbandoActivity.java`:
        - Added an `ImageView` for the traffic light.
        - Implemented logic to switch the image resource on button click.
        - Changed thread sleep duration from 1000ms to 5000ms.
        - Started the thread `hilo`.
    - Updated `activity_probando.xml`:
        - Changed `View` to `ImageView` and set its ID to `myImage`.
        - Set initial background to `circulitonegro`.
    - Renamed `circulito.xml` to `circulitonegro.xml` and changed its color to black.

--- app/src/main/java/com/tuempresa/hilocontador/ProbandoActivity.java
+++ app/src/main/java/com/tuempresa/hilocontador/ProbandoActivity.java
@@ -14,13 +14,15 @@ public class ProbandoActivity extends AppCompatActivity {
     TextView tvTextoEjemplo;
     ImageView myImage;
     ImageView myImage2;
-    ImageView myImageAmarillo;
-    ImageView myImageVerde;
-    ImageView myImageRojo;
+    ImageView myImageAmarillo1;
+    ImageView myImageVerde1;
+    ImageView myImageRojo1;
+    ImageView myImageAmarillo2;
+    ImageView myImageVerde2;
+    ImageView myImageRojo2;
     private boolean encendido = false;
     private int colorActual = 0;
     private int colorActual2 = 0;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,9 +30,16 @@ public class ProbandoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_probando);
         btnHilo = findViewById(R.id.btnHilo);
         tvTextoEjemplo = findViewById(R.id.tvTextoEjemplo);
-        myImage = findViewById(R.id.myImage);
-        myImage2 = findViewById(R.id.myImage2);
 
+
+        myImageAmarillo1 = findViewById(R.id.myImageAmarillo1);
+        myImageVerde1 = findViewById(R.id.myImageVerde1);
+        myImageRojo1 = findViewById(R.id.myImageRojo1);
+        myImageAmarillo2 = findViewById(R.id.myImageAmarillo2);
+        myImageVerde2 = findViewById(R.id.myImageVerde2);
+        myImageRojo2 = findViewById(R.id.myImageRojo2);
+
+        apagartodos();
 
         btnHilo.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,29 +49,45 @@ public class ProbandoActivity extends AppCompatActivity {
                     public void run() {
                         for (int i = 0; i < 10; i++) {
                             final int dec = i;
-
                             runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
                                     tvTextoEjemplo.setText(String.valueOf("Contador: " + dec));
-
-                                    switch (colorActual) {
-                                        case 0:
-                                            myImage.setImageResource(R.drawable.foco_rojo);
-                                            break;
-                                        case 1:
-                                            myImage.setImageResource(R.drawable.foco_amarillo);
-                                            break;
-                                        case 2:
-                                            myImage.setImageResource(R.drawable.foco_verde);
-                                            break;
-                                    }
-                                    colorActual = (colorActual + 1) % 3;
+                                    apagartodos();
+                                    myImageRojo1.setImageResource(R.drawable.foco_rojo);
+                                    myImageVerde2.setImageResource(R.drawable.foco_verde);
                                 }
                             });
                             try {
                                 Thread.sleep(5000);
-
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    tvTextoEjemplo.setText(String.valueOf("Contador: " + dec));
+                                    apagartodos();
+                                    myImageAmarillo1.setImageResource(R.drawable.foco_amarillo);
+                                    myImageAmarillo2.setImageResource(R.drawable.foco_amarillo);
+                                }
+                            });
+                            try {
+                                Thread.sleep(2000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    tvTextoEjemplo.setText(String.valueOf("Contador: " + dec));
+                                    apagartodos();
+                                    myImageVerde1.setImageResource(R.drawable.foco_verde);
+                                    myImageRojo2.setImageResource(R.drawable.foco_rojo);
+                                }
+                            });
+                            try {
+                                Thread.sleep(5000);
                             } catch (InterruptedException e) {
                                 e.printStackTrace();
                             }
@@ -70,56 +95,31 @@ public class ProbandoActivity extends AppCompatActivity {
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-
                             }
                         });
                     }
                 });
                 hilo.start();
-
-                Thread hilo2 = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        for (int i = 0; i < 10; i++) {
-                            final int decc = i;
-
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    tvTextoEjemplo.setText(String.valueOf("Contador: " + decc));
-
-                                    switch (colorActual2) {
-                                        case 0:
-                                            myImage2.setImageResource(R.drawable.foco_verde);
-                                            break;
-                                        case 1:
-                                            myImage2.setImageResource(R.drawable.foco_amarillo);
-                                            break;
-                                        case 2:
-                                            myImage2.setImageResource(R.drawable.foco_rojo);
-                                            break;
-                                    }
-                                    colorActual2 = (colorActual2 + 1) % 3;
-                                }
-                            });
-                            try {
-                                Thread.sleep(5000);
-
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-
-                            }
-                        });
-                    }
-                });
-                hilo2.start();
             }
-
         });
+
+
+    }
+
+    private void apagartodos() {
+        apagar1();
+        apagar2();
+    }
+
+    private void apagar2() {
+        myImageAmarillo2.setImageResource(R.drawable.circulitonegro);
+        myImageVerde2.setImageResource(R.drawable.circulitonegro);
+        myImageRojo2.setImageResource(R.drawable.circulitonegro);
+    }
+
+    private void apagar1() {
+        myImageAmarillo1.setImageResource(R.drawable.circulitonegro);
+        myImageVerde1.setImageResource(R.drawable.circulitonegro);
+        myImageRojo1.setImageResource(R.drawable.circulitonegro);
     }
 }
