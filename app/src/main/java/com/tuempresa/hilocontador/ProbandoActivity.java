749b670660c9cc2e413361bf906a6ea7ae32ee65
riblawaldir <trancosowaldir@gmail.com>
2025-08-27 18:31:16 -0400

    Refactor: Implement toggling traffic light
    This commit introduces functionality to toggle a traffic light image
    between red and black on button click.
    
    Key changes:
    - Added `foco_rojo.xml` drawable for the red light.
    - Modified `ProbandoActivity.java`:
        - Added an `ImageView` for the traffic light.
        - Implemented logic to switch the image resource on button click.
        - Changed thread sleep duration from 1000ms to 5000ms.
        - Started the thread `hilo`.
    - Updated `activity_probando.xml`:
        - Changed `View` to `ImageView` and set its ID to `myImage`.
        - Set initial background to `circulitonegro`.
    - Renamed `circulito.xml` to `circulitonegro.xml` and changed its color to black.

--- app/src/main/java/com/tuempresa/hilocontador/ProbandoActivity.java
+++ app/src/main/java/com/tuempresa/hilocontador/ProbandoActivity.java
@@ -2,6 +2,7 @@ package com.tuempresa.hilocontador;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +12,8 @@ import androidx.core.view.WindowInsetsCompat;
 public class ProbandoActivity extends AppCompatActivity {
     Button btnHilo;
     TextView tvTextoEjemplo;
+    ImageView myImage;
+    private boolean encendido = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +21,8 @@ public class ProbandoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_probando);
         btnHilo = findViewById(R.id.btnHilo);
         tvTextoEjemplo = findViewById(R.id.tvTextoEjemplo);
+        myImage = findViewById(R.id.myImage);
+
         btnHilo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -25,15 +30,24 @@ public class ProbandoActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         for (int i = 0; i < 10; i++) {
-                            int dec = 1;
+                            final int dec = i;
+
                             runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
                                     tvTextoEjemplo.setText(String.valueOf("Contador: " + dec));
+
+                                    if (encendido) {
+                                        myImage.setImageResource(R.drawable.circulitonegro);
+                                    } else {
+                                        myImage.setImageResource(R.drawable.foco_rojo);
+                                    }
+                                    encendido = !encendido;
                                 }
                             });
                             try {
-                                Thread.sleep(1000);
+                                Thread.sleep(5000);
+
                             } catch (InterruptedException e) {
                                 e.printStackTrace();
                             }
@@ -41,11 +55,12 @@ public class ProbandoActivity extends AppCompatActivity {
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                tvTextoEjemplo.setText("Proceso de hilo finalizado");
+
                             }
                         });
                     }
                 });
+                hilo.start();
             }
         });
     }
